28e75646aec8d358a43ff8f7d6214d0a3c26885b
Kendell R <KTibow@users.noreply.github.com>
2022-01-27 15:17:46 -0800

    Update app.js

--- apps/school/app.js
+++ apps/school/app.js
@@ -1,77 +1,66 @@
+const blockDay = [
+    [7, 50, "am"],
+    [8, 20, "am"],
+    [10, 10, "am"],
+    [11, 13, "am"],
+    [11, 43, "am"],
+    [12, 35, "am"],
+    [2, 25, "pm"],
+];
+const wednesday = [
+    [7, 50, "am"],
+    [8, 32, "am"],
+    [9, 18, "am"],
+    [10, 4, "am"],
+    [10, 38, "am"],
+    [11, 24, "am"],
+    [12, 10, "am"],
+    [12, 55, "am"],
+];
 const schedule = {
     // [end hour, end min, am/pm]
-    1: [
-        [8, 20, "am"],
-        [10, 10, "am"],
-        [11, 13, "am"],
-        [11, 43, "am"],
-        [12, 35, "am"],
-        [2, 25, "pm"],
-    ],
-    2: [
-        [8, 20, "am"],
-        [10, 10, "am"],
-        [11, 13, "am"],
-        [11, 43, "am"],
-        [12, 35, "am"],
-        [2, 25, "pm"],
-    ],
-    3: [
-        [8, 32, "am"],
-        [9, 18, "am"],
-        [10, 4, "am"],
-        [10, 38, "am"],
-        [11, 24, "am"],
-        [12, 10, "am"],
-        [12, 55, "am"],
-    ],
-    4: [
-        [8, 20, "am"],
-        [10, 10, "am"],
-        [11, 13, "am"],
-        [11, 43, "am"],
-        [12, 35, "am"],
-        [2, 25, "pm"],
-    ],
-    5: [
-        [8, 20, "am"],
-        [10, 10, "am"],
-        [11, 13, "am"],
-        [11, 43, "am"],
-        [12, 35, "am"],
-        [2, 25, "pm"],
-    ],
+    1: blockDay,
+    2: blockDay,
+    3: wednesday,
+    4: blockDay,
+    5: blockDay,
 };
 
 function padMinute(minute) {
     return minute < 10 ? "0" + minute : minute;
 }
 
+function convertTo24Hr(hours, meridiem) {
+    return hours + (meridiem == "pm" ? 12 : 0);
+}
+
+function convertTo12Hr(hours) {
+    return hours % 12 || 12;
+}
+
 function findPeriod() {
-    g.clear();
+    g.setFontAlign(0, 0);
     const weekday = new Date().getDay();
     const hour = new Date().getHours();
     const minute = new Date().getMinutes();
     const todaySchedule = schedule[weekday];
     for (let period of todaySchedule) {
-        const periodEndHour = period[0] + (period[2] == "pm" ? 12 : 0);
+        const periodEndHour = convertTo24Hr(period[0], period[2]);
         const periodEndMinute = period[1];
-        if (
-            hour < periodEndHour ||
-            (hour == periodEndHour && minute < periodEndMinute)
-        ) {
+        if (hour < periodEndHour || (hour == periodEndHour && minute < periodEndMinute)) {
             displayPeriod(period, periodEndHour, periodEndMinute);
             return;
         }
     }
-    g.setFontAlign(0, 0);
+    g.clearRect(0, g.getWidth() / 2 - 12.5, g.getWidth(), g.getHeight() / 2 + 12.5);
     g.setFont("Vector", 25);
     g.drawString("[nothing upcoming]", g.getWidth() / 2, g.getHeight() / 2);
-    g.setFont("Vector", 40);
+    g.clearRect(0, g.getWidth() / 2 + 80 - 40, g.getWidth(), g.getHeight() / 2 + 80 + 40);
+    g.setFont("Vector", 80);
     g.drawString(
-        `(${hour % 12}:${padMinute(minute)})`,
+        `(${convertTo12Hr(hour)}:${padMinute(minute)})`,
         g.getWidth() / 2,
-        g.getHeight() / 2 + 50
+        g.getHeight() / 2 + 80
     );
 }
 
@@ -80,34 +69,30 @@ function displayPeriod(period, periodEndHour, periodEndMinute) {
     const minute = new Date().getMinutes();
     const seconds = new Date().getSeconds();
     let timeLeft = Math.floor(
-        (periodEndHour - hour) * 60 * 60 +
-            (periodEndMinute - minute) * 60 -
-            seconds
+        (periodEndHour - hour) * 60 * 60 + (periodEndMinute - minute) * 60 - seconds
     );
-    g.setFontAlign(0, 0);
     g.setFont("Vector", 50);
+    g.clearRect(0, g.getWidth() / 2 - 25, g.getWidth(), g.getHeight() / 2 + 25);
     g.drawString(
         `${Math.floor(timeLeft / 60)}m ${timeLeft % 60}s`,
         g.getWidth() / 2,
         g.getHeight() / 2
     );
-    g.setFont("Vector", 20);
-    g.drawString(
-        `(${hour % 12}:${padMinute(minute)} -> ${
-            periodEndHour % 12
-        }:${padMinute(periodEndMinute)})`,
-        g.getWidth() / 2,
-        g.getHeight() / 2 + 50
-    );
+    g.setFont("Vector", 30);
+    const currentTime = `${convertTo12Hr(hour)}:${padMinute(minute)}`;
+    const endTime = `${convertTo12Hr(periodEndHour)}:${padMinute(periodEndMinute)})`;
+    g.clearRect(0, g.getWidth() / 2 + 50 - 15, g.getWidth(), g.getHeight() / 2 + 50 + 15);
+    g.drawString(`(${currentTime} -> ${endTime}`, g.getWidth() / 2, g.getHeight() / 2 + 50);
     if (timeLeft <= 30) {
         Bangle.setLCDPower(1);
     }
     if (timeLeft == 30) {
-        Bangle.buzz(200);
+        Bangle.buzz(250);
     }
     if (timeLeft == 5) {
         Bangle.buzz(500);
     }
 }
 
+g.clear();
 const interval = setInterval(findPeriod, 1000);
