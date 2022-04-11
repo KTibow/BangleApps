a4e921021a79258619b8bcb4da2edae200649060
Kendell R <KTibow@users.noreply.github.com>
2022-04-11 18:25:04 -0400

    Initial bangled

---
+++ apps/bangled/app.js
@@ -0,0 +1,19 @@
+var Layout = require("Layout");
+var layout = new Layout({
+  type: "h",
+  c: [
+    {
+      type: "img",
+      pad: 4,
+      src: require("heatshrink").decompress(
+        atob(
+          "ikUwYFCgVJkgMDhMkyVJAwQFCAQNAgESAoQCBwEBBwlIgAFDpNkyAjDkm/5MEBwdf+gUEl/6AoVZkmX/oLClv6pf+DQn1/4+E3//0gFBkACBv/SBYI7D5JiDLJx9CBAR4CAoWQQ4Z9DgAA=="
+        )
+      ),
+    },
+    { type: "txt", font: "6x8:2", label: "All ok" },
+  ],
+});
+
+g.clear();
+layout.render();
