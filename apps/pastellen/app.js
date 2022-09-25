e95c9e03e36d8a17f21e6a0ed1b99e396f2eb4c2
Kendell R <KTibow@users.noreply.github.com>
2022-09-25 16:01:03 -0700

    update some stuff

--- apps/pastellen/app.js
+++ apps/pastellen/app.js
@@ -1,19 +1,24 @@
 let lastInfoUpdate = 0;
 let lastTimeUpdate = 0;
-require("f_recursive_clock").add(Graphics);
-require("f_recursive_text").add(Graphics);
+require("f_recursive_clock.js").add(Graphics);
+require("f_recursive_text.js").add(Graphics);
 const hoursTo12 = (hour) => hour % 12 || 12;
+function drawTime(y, now) {
+  const buf = Graphics.createArrayBuffer(240, 80, 1, { msb: true });
+  const hourStr = hoursTo12(now.getHours());
+  const minStr = now.getMinutes().toString().padStart(2, "0");
+  const currentTime = `${hourStr}:${minStr}`;
+  buf.setFont("RecursiveClock");
+  buf.setFontAlign(0, 0);
+  buf.setColor(g.theme.fg);
+  buf.drawString(currentTime, 120, 40);
+  g.drawImage(buf, 0, y);
+}
 setInterval(() => {
-  let now = Date.now();
-  let currentMinute = Math.floor(Date.now() / 1000 / 60);
+  const now = new Date();
+  const currentMinute = Math.floor(now.getTime() / 1000 / 60);
   if (currentMinute != lastTimeUpdate) {
-    let buf = Graphics.createArrayBuffer(240, 80, 1, { msb: true });
-    let currentTime = `${hoursTo12(now.getHour())}:${now.getMinute()}`;
-    buf.setFont("RecursiveClock");
-    buf.setFontAlign(0, 0);
-    buf.setColor(g.theme.fg);
-    buf.drawString(currentTime, 120, 40);
-    g.drawImage(buf, 0, 80);
+    drawTime(80, now);
     lastTimeUpdate = currentMinute;
   }
 }, 100);
