6170497280a052b305046ab5c3f6eac8aa570520
Kendell R <KTibow@users.noreply.github.com>
2022-09-27 17:43:33 -0700

    weather?

--- apps/flicc/app.js
+++ apps/flicc/app.js
@@ -2,6 +2,9 @@ let lastInfoUpdate = 0;
 let lastTimeUpdate = 0;
 require("f_recursive_clock.js").add(Graphics);
 require("f_recursive_text.js").add(Graphics);
+const weatherIcons = require("weather_icons.js");
+let weatherData = JSON.parse(require("Storage").open("weather.json", "r").readLine() || "[]");
+
 const hoursTo12 = (hour) => hour % 12 || 12;
 function drawTime(y, now) {
   const buf = Graphics.createArrayBuffer(240, 80, 1, { msb: true });
@@ -11,9 +14,14 @@ function drawTime(y, now) {
   buf.setFont("RecursiveClock").setFontAlign(0, 0).drawString(currentTime, 120, 40);
   g.setColor(g.theme.fg).drawImage(buf, 0, y);
 }
-function drawBottomWidgets(y, now) {
+function drawWidgets(y, now, charging) {
   g.clearRect(0, y - 21, 240, y);
-  g.setColor(g.theme.fg).drawPoly([
+  if (charging) {
+    g.setColor(100 / 255, 200 / 255, 100 / 255);
+  } else {
+    g.setColor(g.theme.fg);
+  }
+  g.drawPoly([
     10,
     y,
     20,
@@ -34,7 +42,6 @@ function drawBottomWidgets(y, now) {
     y,
   ]);
   const battery = E.getBattery();
-  const charging = Bangle.isCharging();
   const batteryHeight = (battery / 100) * 20;
   for (let currentHeight = 1; currentHeight < batteryHeight; currentHeight++) {
     const drawingAt = y - currentHeight;
@@ -45,14 +52,9 @@ function drawBottomWidgets(y, now) {
     } else {
       g.setColor(100 / 255, 100 / 255, 200 / 255);
     }
-    g.setColor(g.theme.bgH).drawLine(11, drawingAt, 19, drawingAt);
+    g.drawLine(11, drawingAt, 19, drawingAt);
   }
-  g.setColor(g.theme.fg)
-    .setFont("RecursiveText")
-    .setFontAlign(-1, 1)
-    .drawString(Math.floor(battery) + (charging ? "+" : "%"), 20, y);
 
-  const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][now.getDay()];
   const monthName = [
     "Jan",
     "Feb",
@@ -70,24 +72,65 @@ function drawBottomWidgets(y, now) {
   const dayOfMonth = now.getDate().toString();
   g.setColor(g.theme.fg)
     .setFont("RecursiveText")
-    .setFontAlign(1, 1)
-    .drawString(`${dayOfWeek} ${monthName} ${dayOfMonth}`, 230, y);
+    .setFontAlign(1, -1)
+    .drawString(`${monthName} ${dayOfMonth}`, 230, y - 20);
+
+  const datestamp =
+    now.getFullYear() +
+    "-" +
+    (now.getMonth() + 1).toString().padStart(2, "0") +
+    "-" +
+    now.getDate().toString().padStart(2, "0");
+  const currentWeather = weatherData.find((weather) => weather.datetime == datestamp);
+  if (currentWeather) {
+    g.setColor(g.theme.fg).setFont("RecursiveText").setFontAlign(-1, -1);
+    const info = currentWeather.low + " - " + currentWeather.high;
+    let x = 120 - g.getStringWidth(info) / 2;
+    const icon = weatherIcons[currentWeather.icon];
+    if (icon) {
+      x += 19 / 2;
+    }
+    g.drawImage(icon, x - 20, 60);
+    g.drawString(info, x, 60);
+  }
+
+  g.clearRect(0, 160, 240, 160 + 25);
+  g.setColor(g.theme.fg)
+    .setFont("RecursiveText")
+    .setFontAlign(0, -1)
+    .drawString(`Steps: ${Bangle.getStepCount()}`, 120, 160);
 }
-setInterval(() => {
+
+let skippedClearing = false;
+function draw() {
+  if (!Bangle.isLCDOn()) {
+    if (Date.now() % (1000 * 60) < 100) {
+      skippedClearing = true;
+    }
+    return;
+  }
   const now = new Date();
   const currentMinute = Math.floor(now.getTime() / 1000 / 60);
+  const minutePortion = now.getTime() % (1000 * 60);
   if (currentMinute != lastTimeUpdate) {
     drawTime(80, now);
     lastTimeUpdate = currentMinute;
   }
-  const minutePortion = now.getTime() % (1000 * 60);
-  if (minutePortion < 100) {
+  if (minutePortion < 100 || skippedClearing) {
     g.setColor(g.theme.bg).drawLine(0, 160, 240, 160);
+    skippedClearing = false;
   }
   g.setColor(g.theme.bgH).drawLine(0, 160, (minutePortion / 1000 / 60) * 240, 160);
   if (currentMinute - lastInfoUpdate > 5) {
-    drawBottomWidgets(230, now);
+    drawWidgets(230, now, Bangle.isCharging());
     lastInfoUpdate = currentMinute;
   }
+}
+
+setInterval(() => {
+  draw();
 }, 100);
+Bangle.on("charging", (charging) => {
+  drawWidgets(230, new Date(), charging);
+});
 g.clear();
